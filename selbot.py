0f22d3a39af2e771c52f58c923a928a53eca1855
Nathan Yee <32085568+NathanYee412@users.noreply.github.com>
2020-09-15 11:05:11 -0700

    Throws pokeballs based on rarity

--- selbot.py
+++ selbot.py
@@ -1,29 +1,31 @@
 from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+
 import time
 import random
 
 
 
-#Enter links and credentials
-#username and pw  
-email = ""
-password = ""
-
-# (You will need to get the login redirect link for the script to work)
-#Link to discord channel with bot
-discordLink = "https://discord.com/login?redirect_to=%2Fchannels%2F755173566605164615%2F755245005802700862"
-
-
-#download chromedriver.exe and extract to the .exe to the C drive 
+#download chromedriver.exe and extract to the .exe to the C drive
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
+#email and pw
+email = ""
+password = ""
 
-#login to discord 
+# your discord @
+DISCORD_AT = ""
+
+#link to discord text channel
+discordLink = ""
+
+#login to discord
 def discordLogin(email, password):
     driver.get(discordLink)
-
+    time.sleep(5)
     elemEmail = driver.find_element_by_name("email")
     elemEmail.send_keys(email)
 
@@ -32,43 +34,122 @@ def discordLogin(email, password):
     elemPw.send_keys(Keys.RETURN)
     time.sleep(10)
 
-#check rarity 
+#check rarity
 #WIP
 def checkRarity():
-    rarity = driver.find_element_by_css_selector("span.embedFooterText-28V_Wb")
+    message_list = WebDriverWait(driver, timeout=9).until(lambda d: d.find_elements_by_class_name("container-1ov-mD"))
+    last_element = message_list[-1]
 
-#shop buy
-def shopBuy():
-    p = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div/div/div/div/div[3]/div[2]')
-    p.send_keys(";shop buy 1 10")
-    p.send_keys(Keys.RETURN) 
-    
+    element = WebDriverWait(last_element, timeout=5).until(lambda d: d.find_elements_by_css_selector("span.embedFooterText-28V_Wb"))
 
-#catch function 
-def catchP():
-    p = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div/div/div/div/div[3]/div[2]')
+    if len(element) == 0:
+        return "Error"
+    element = element[0]
+
+    text = element.text.split()
+    print(element.text)
+    return text[0]
+
+def spawn(p):
     p.send_keys(";p")
     p.send_keys(Keys.RETURN)
-    time.sleep(random.randint(3,5))
+
+def pb(p):
     p.send_keys("pb")
     p.send_keys(Keys.RETURN)
-    time.sleep(random.randint(9,12))
+
+
+def gb(p):
+    p.send_keys("gb")
+    p.send_keys(Keys.RETURN)
+
+def ub(p):
+    p.send_keys("ub")
+    p.send_keys(Keys.RETURN)
+
+def mb(p):
+    p.send_keys("mb")
+    p.send_keys(Keys.RETURN)
+
+def getXpath(i):
+    return "/html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div/main/div[1]/div/div/div/div[" + str(i) + "]"
+
+# can do something better than ping to alert you of captchan since it doesn't actually give you a notif
+def ping(p):
+    p.send_keys(DISCORD_AT)
+    p.send_keys(Keys.TAB)
+    p.send_keys(Keys.RETURN)
+
+
+
+#PB index = 13
+#UB index = 16
+#GB index = 18
+
+#return list of pb count
+def getPbCount():
+    message_list = WebDriverWait(driver, timeout=9).until(lambda d: d.find_elements_by_class_name("container-1ov-mD"))
+    last_element = message_list[-1]
+
+    element = WebDriverWait(last_element, timeout=5).until(lambda d: d.find_elements_by_css_selector("span.embedFooterText-28V_Wb"))
+
+    if len(element) == 0:
+        return "Error"
+    element = element[0]
+
+    text = element.text.split()
+    print(element.text)
+
+    pbList = [text[13], text[18], text[16]]
+    return pbList
+
+#shop buy pokeballs 
+def shopBuy(pbList):
+    if int(pbList[0]) <= 5:
+        p.send_keys(";shop buy 1 5")
+        p.send_keys(Keys.RETURN)
+    if int(pbList[1]) <= 5:
+        p.send_keys(";shop buy 2 5")
+        p.send_keys(Keys.RETURN)
+    if int(pbList[2]) <= 5:
+        p.send_keys(";shop buy 3 5")
+        p.send_keys(Keys.RETURN)
+
+    time.sleep(random.randint(4,6))
+
 
 
 #login to discord
 discordLogin(email, password)
-
-
 #catch pokemon in infinite loop
-temp = True
-count = 1
-while(temp):
-    
-    #catch pokemon
-    catchP()
-    count = count + 1
-    print(count)
+p = driver.find_element_by_xpath("/html/body/div/div[2]/div/div[2]/div/div/div/div[2]/div[2]/div/main/form/div/div/div/div/div[3]/div[2]")
 
-    #buy pokeballs every 10 uses
-    if count % 10 == 0:
-        shopBuy()
+x = True
+while(x):
+    spawn(p)
+    time.sleep(random.randint(1,2) + random.random())
+    rarity = checkRarity()
+    print(rarity)
+
+    #get pb list 
+    pbList = getPbCount()
+
+    time.sleep(random.randint(1,3) + random.random())
+    if rarity == "Common":
+        pb(p)
+    elif rarity == "Uncommon":
+        gb(p)
+    elif rarity == "Rare":
+        gb(p)
+    elif rarity == "Super":
+        ub(p)
+    elif rarity =="Error":
+        ping(p)
+        quit()
+    else:
+        mb(p)
+
+    time.sleep(random.randint(9,12) + random.random())
+
+    #buy more pb
+    shopBuy(pbList)
